46cdc5250d1bea3e6a8dc3dcefca797843f886a3
Glen Sale and Leilani Leach <glen@>
2018-10-17 16:17:33 -0700

    uses new features of selectors and pop up window

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $(".open").on("click", function(){
+    $(".popup-overlay, .popup-content").addClass("active");
+  });
+  $(".close, .popup-overlay").on("click", function(){
+    $(".popup-overlay, .popup-content").removeClass("active");
+  });
+});
